35e09e41a118de22e66fd18a9c86af8513d7435c
Dominic <meiserdo@web.de>
2017-07-10 19:34:55 +0200

    add gpl3 license everywhere

--- cli/src/msrd0/matrix/client/cli/Main.java
+++ cli/src/msrd0/matrix/client/cli/Main.java
@@ -1,3 +1,21 @@
+/*
+ matrix-client-cli
+ Copyright (C) 2017 Dominic Meiser
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0>.
+*/
+
 package msrd0.matrix.client.cli;
 
 import static java.util.concurrent.TimeUnit.*;
@@ -7,8 +25,9 @@ import java.net.URI;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import com.google.common.base.Stopwatch;
 import msrd0.matrix.client.*;
+
+import com.google.common.base.Stopwatch;
 import org.slf4j.*;
 
 public class Main
