e70b63a6d4551607ac8cb022a28e2a9e42b10a6a
Dominic <meiserdo@web.de>
2017-07-21 16:20:20 +0200

    Merge branch 'events' into master
    Squashed commit of the following:
    
    commit 593400357410bc3657c3fb3db81ba812b7b1075a
    Author: Julius Lehmann <julius@devpi.de>
    Date:   Fri Jul 21 16:13:26 2017 +0200
    
        Move Matrix Olm library to separate location
    
        Signed-off-by: Julius Lehmann <julius@devpi.de>
    
    commit 7beb4e3d98cb45b15c0082b626f21be955e6597d
    Merge: b1e1246 8bf7ec4
    Author: Dominic <meiserdo@web.de>
    Date:   Fri Jul 21 13:34:34 2017 +0200
    
        Merge branch 'master' into events
    
    commit b1e1246251ac5badc20fca65d5761b730bd80d0c
    Author: Julius Lehmann <julius@devpi.de>
    Date:   Fri Jul 21 12:30:56 2017 +0200
    
        Add native libraries to gitignore
    
        Signed-off-by: Julius Lehmann <julius@devpi.de>
    
    commit db148fd073932d0597e6f618815432dc7aa47ad7
    Author: Julius Lehmann <julius@devpi.de>
    Date:   Fri Jul 21 12:29:49 2017 +0200
    
        Move olm encryption to this client
    
        Signed-off-by: Julius Lehmann <julius@devpi.de>
    
    commit 929743e57d9a5ca8027ddf7c9b82054ac7c9c17d
    Merge: ea8d231 5c959eb
    Author: Julius Lehmann <julius@devpi.de>
    Date:   Fri Jul 21 09:58:41 2017 +0200
    
        Merge branch 'master' into events
    
    commit ea8d2319818c9a20c56d28e437fd6ca980dfc7f9
    Author: Julius Lehmann <julius@devpi.de>
    Date:   Fri Jul 21 09:58:07 2017 +0200
    
        Add serialization
    
        Signed-off-by: Julius Lehmann <julius@devpi.de>
    
    commit d497082acc6562426b9ac78bf294cba078f1d90e
    Merge: 15e9235 b56ca65
    Author: Julius Lehmann <julius@devpi.de>
    Date:   Tue Jul 18 12:20:18 2017 +0200
    
        Merge branch 'master' into events
    
    commit 15e923503d64b9459999d9670b34555d588a9df8
    Author: Julius Lehmann <julius@devpi.de>
    Date:   Tue Jul 18 12:19:04 2017 +0200
    
        Improved EventEncryptor interface to meet Olm encryption requirements
    
        Signed-off-by: Julius Lehmann <julius@devpi.de>

---
+++ src/msrd0/matrix/client/encryption/roomencryptor.kt
@@ -0,0 +1,68 @@
+/*
+ * matrix-client
+ * Copyright (C) 2017 Julius Lehmann
+ *  
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
+package msrd0.matrix.client.encryption
+
+import com.beust.klaxon.JsonObject
+
+interface RoomEncryptor
+{
+	/**
+	 * Add new secret to this RoomEncryptor
+	 *
+	 * This function adds secrets previously exchanged with Olm to this instance of RoomEncryptor. This may include
+	 * shared secrets to decrypt messages or session ids to initialize an underlying encryption object.
+	 *
+	 * The map should contain all all key-value pairs from the decrypted Json.
+	 *
+	 * @param secrets the map containing all secrets transmitted by olm
+	 */
+	fun addSecrets(secrets : Map<String, String> = mapOf())
+	
+	/**
+	 * Enrypts a room event
+	 *
+	 * After encryption has finished, the result should be sent as is to the corresponding room. A client/server may add
+	 * additional properties **outside** of ```content```.
+	 */
+	fun getEncryptedJson(event : JsonObject) : JsonObject
+	
+	/**
+	 * Decrypts a room event
+	 *
+	 * If an event can not be decrypted correctly, this method will throw an exception. To be able to decrypt all events
+	 * successfully all known secrets should be passed to [addSecrets] to ensure successful decryption of all events.
+	 */
+	fun getDecryptedJson(event : JsonObject) : JsonObject
+	
+	/**
+	 * Returns the secrets that need to be transmitted to other devices
+	 *
+	 * Other devices might need additional information to be able to decrypt events that have been encrypted previously
+	 * by this class.
+	 */
+	fun getSecrets() : JsonObject
+	
+	/**
+	 * Indicates whether underlying encryption parameters have changed.
+	 *
+	 * If so, [getSecrets] should be called and transmitted to all participating devices.
+	 */
+	fun hasPendingSecrets() : Boolean
+}
--- test/src/msrd0/matrix/client/encryption/test/olm.kt
+++ test/src/msrd0/matrix/client/encryption/test/olm.kt
@@ -1,30 +1,29 @@
 /*
  * matrix-client
- * Copyright (C) 2017  Julius Lehmann
- *  
+ * Copyright (C) 2017 Julius Lehmann
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
-package msrd0.matrix.client.event
 
-import com.beust.klaxon.JsonObject
+package msrd0.matrix.client.encryption.test
 
-interface EventEncryptor
+import msrd0.matrix.client.encryption.olm
+import org.testng.annotations.Test
+
+@Test
+fun version()
 {
-	fun getEncryptedJson(event : JsonObject) : JsonObject
-	fun getDecryptedJson(event : JsonObject) : JsonObject
-	fun exchangeKeys() : JsonObject
-	fun addDeviceKeys(userId : String, deviceKey : String, identityKey : String, vararg additionalKeys : String)
-	fun uploadKeysJson() : JsonObject
+	olm.olmLibVersion
 }
