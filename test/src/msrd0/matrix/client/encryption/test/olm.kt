d211233718f7219b0d5c9b7cf849eab411f2a89e
Dominic <meiserdo@web.de>
2017-07-30 17:12:12 +0200

    also include java files in license check && update licenses

--- test/src/msrd0/matrix/client/encryption/test/olm.kt
+++ test/src/msrd0/matrix/client/encryption/test/olm.kt
@@ -1,10 +1,11 @@
 /*
  * matrix-client
+ * Copyright (C) 2017 Dominic Meiser
  * Copyright (C) 2017 Julius Lehmann
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,8 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0>.
  */
 
 package msrd0.matrix.client.encryption.test
